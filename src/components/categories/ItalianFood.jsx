5bee7295827f9fb14df5f7bb308a70ba01b155de
Stefani Ilieva <stephanieilieva1995@gmail.com>
2022-05-15 19:29:35 +0300

    feat: add new image tags

--- src/components/categories/ItalianFood.jsx
+++ src/components/categories/ItalianFood.jsx
@@ -1,24 +1,41 @@
-import Card from "../card-categorie/Card.jsx"
+import Card from "../card-categorie/Card.jsx";
 // import { useState } from "react";
 import styles from "./ItalianFood.module.scss";
-import {  Container, Grid} from "@mui/material";
+import { Container, Grid } from "@mui/material";
 
 export default function ItalianFood({ cards = [] }) {
-
   return (
     <div className={styles.wrapperHome}>
       <Container className={styles.container} maxWidth="xl">
-      <Grid container className={styles.gridContainer} sx={{position:'relative', left: '154px'}}>
+        <Grid
+          container
+          className={styles.gridContainer}
+          sx={{ position: "relative", left: "154px" }}
+        >
           <Grid item className={styles.titleWrapper}>
             <div className={styles.title}>
-            🍝 MAYBE SOME ITALIAN?
+              <img
+                src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/285/spaghetti_1f35d.png"
+                alt="spaghetti"
+                width="60"
+                height="60"
+              />{" "}
+              MAYBE SOME ITALIAN?
             </div>
           </Grid>
         </Grid>
-        <Grid sx={{mt: 3}} container spacing={2} item className={styles.cards} xl>
+        <Grid
+          sx={{ mt: 3 }}
+          container
+          spacing={2}
+          item
+          className={styles.cards}
+          xl
+        >
           {cards.map((card) => (
             <Grid item xs={3} md={3}>
-              <Card lassName={styles.card}
+              <Card
+                lassName={styles.card}
                 name={card.name}
                 tags={card.tags}
                 mediaUrl={card.mediaUrl}
--- src/components/categories/Seafood.jsx
+++ src/components/categories/Seafood.jsx
@@ -1,21 +1,37 @@
 import Card from "../card-categorie/Card.jsx";
 // import { useState } from "react";
-import styles from './SeaFood.module.scss'
+import styles from "./SeaFood.module.scss";
 import { Container, Grid } from "@mui/material";
 
 export default function SeaFood({ cards = [] }) {
- 
   return (
     <div className={styles.wrapperHome}>
       <Container className={styles.container} maxWidth="xl">
-        <Grid container className={styles.gridContainer} sx={{position:'relative', left: '154px'}}>
+        <Grid
+          container
+          className={styles.gridContainer}
+          sx={{ position: "relative", left: "154px" }}
+        >
           <Grid item className={styles.titleWrapper}>
             <div className={styles.title}>
-            🌊DOWN FOR SEAFOOD TONIGHT?
+              <img
+                src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/285/water-wave_1f30a.png"
+                alt="wave"
+                width="50"
+                height="50"
+              />
+              DOWN FOR SEAFOOD TONIGHT?
             </div>
           </Grid>
         </Grid>
-        <Grid sx={{mt: 3}} container spacing={2} item className={styles.cards} xl>
+        <Grid
+          sx={{ mt: 3 }}
+          container
+          spacing={2}
+          item
+          className={styles.cards}
+          xl
+        >
           {cards.map((card) => (
             <Grid item xs={3} md={3}>
               <Card
